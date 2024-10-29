c47b443f76e26e96af1075966d2617acdf333471
AD <154968000+dangol-anish@users.noreply.github.com>
2024-10-29 20:56:35 +0545

    test: api endpoint

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -1,16 +1,16 @@
 "use client"
+import React, { Suspense } from "react";
 import DetailsContent from "@/components/DetailsPage/DetailsContent";
 import DetailsHeader from "@/components/DetailsPage/DetailsHeader";
 import { useSearchParams } from "next/navigation";
 
-export default function Details() {
+function DetailsComponent() {
   const searchParams = useSearchParams();
   const title = searchParams.get("title");
   const explanation = searchParams.get("explanation");
   const url = searchParams.get("url");
   const hdurl = searchParams.get("hdurl");
   const date = searchParams.get("date");
-
 
   if (!title || !explanation || !url || !hdurl) {
     return <div>Error: Missing data in the URL parameters.</div>;
@@ -20,12 +20,20 @@ export default function Details() {
     <div className="h-[95%] md:h-[90%] w-[90%] md:w-[60%] lg:w-[45%] ">
       <DetailsHeader />
       <DetailsContent
-      date={date}
-      title={title}
-      explanation={explanation}
-      url={url}
-      hdurl={hdurl}
+        date={date}
+        title={title}
+        explanation={explanation}
+        url={url}
+        hdurl={hdurl}
       />
     </div>
+  );
+}
+
+export default function Details() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DetailsComponent />
+    </Suspense>
   );
 }
