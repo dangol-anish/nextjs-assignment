dcdd48d57ffe16d45a4f1d0b8863df863e71f57a
AD <154968000+dangol-anish@users.noreply.github.com>
2024-10-29 16:49:01 +0545

    Design: Main Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import MainPageContent from "@/components/MainPageContent";
 import MainPageFooter from "@/components/MainPageFooter";
 import MainPageHeader from "@/components/MainPageHeader"
 
@@ -6,17 +7,7 @@ export default function Home() {
     <>
   <div className="h-[95%] md:h-[90%] w-[90%] md:w-[60%] lg:w-[45%]">
     <MainPageHeader/>
-    <div className="bg-green-500 w-full h-[80%] grid grid-cols-1 md:grid-cols-3 gap-3 overflow-y-auto">
-      <div className="bg-white h-32 md:h-full "></div>
-      <div className="bg-white h-32 md:h-full"></div>
-      <div className="bg-white h-32 md:h-full"></div>
-      <div className="bg-white h-32 md:h-full"></div>
-      <div className="bg-white h-32 md:h-full"></div>
-      <div className="bg-white h-32 md:h-full"></div>
-      <div className="bg-white h-32 md:h-full"></div>
-      <div className="bg-white h-32 md:h-full"></div>
-      <div className="bg-white h-32 md:h-full"></div>
-    </div>
+    <MainPageContent/>
     <MainPageFooter/>
   </div>
     </>
---
+++ components/MainPageContent.tsx
@@ -0,0 +1,168 @@
+import Link from "next/link";
+import React from "react";
+
+const MainPageContent = () => {
+  return (
+    <div className=" w-full h-[80%] grid grid-cols-1 md:grid-cols-3 gap-3 overflow-y-auto">
+     <Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+<Link
+  href="/details"
+  className="relative h-32 md:h-full bg-default_state text-white rounded-2xl overflow-hidden shadow-lg hover:opacity-80"
+>
+  <img
+    src="https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=2859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="Background Image"
+    className="absolute inset-0 w-full h-full object-cover"
+    loading="lazy"
+  />
+  <div className="absolute bottom-0 p-6 bg-gradient-to-t from-black via-transparent to-transparent w-full">
+    <h3 className="text-sm font-semibold line-clamp-1">Nozomi: Earth and Moon</h3>
+    <p className="text-[10px] mt-2 line-clamp-2">
+      Japan launched its first mission to orbit Mars, Nozomi (Hope), on...
+    </p>
+  </div>
+</Link>
+
+
+
+  
+    </div>
+  );
+};
+
+export default MainPageContent;
